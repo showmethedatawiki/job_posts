1d48068c9c4ea8a78800038c5a995fc6bf4b3639
abdella.mohamed.idris <abdella.mohamed.idris@gmail.com>
2022-01-26 10:21:42 +0000

    update path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,8 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import job_posts
-#sys.path.insert(0, os.path.abspath('../../my_package'))
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath('job_posts')))
 
 # -- Project information -----------------------------------------------------
 
